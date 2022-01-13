4acd9922392f01bfc0ad8a011ce543d5be45e76d
Wiktor Musial <wiktormusial@icloud.com>
2022-01-13 15:52:06 +0100

    Create Note model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from utils.models import TimeStampedModel
 
 # Create your models here.
+class Note(TimeStampedModel):
+    slug = models.SlugField(blank=True)
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    category = models.ForeignKey(
+        'Category',
+        on_delete = models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+    is_archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Note"
+        verbose_name_plural = "Notes"
+
+class Category(TimeStampedModel):
+    pass
