feaa6bf916516ac4ae31312f845b5d7ae9547cca
Wiktor Musial <wiktormusial@icloud.com>
2022-01-14 23:43:30 +0100

    Create views for Notes and Categories

--- note/views.py
+++ note/views.py
@@ -1,25 +1,30 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 from .models import Note, Category
-from .serializers import NoteSerializer
+from .serializers import NoteSerializer, CategorySerializer
 # Create your views here.
 
 class NotesViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    # permission_classes = [IsAuthenticated]
     serializer_class = NoteSerializer
 
-    def get_queryset(self, **kwargs):
-        token = self.kwargs['token']
-        author = Token.objects.get(key=token).user_id
+    def get_queryset(self, *args, **kwargs):
+        author = self.request.user.id
         notes = Note.objects.all().filter(author=author)
 
         return notes
 
-class CategoriesViewSet():
-    pass
+class CategoriesViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    # permission_classes = [IsAuthenticated]
+    serializer_class = CategorySerializer
+
+    def get_queryset(self, *args, **kwargs):
+        author = self.request.user.id
+        categories = Category.objects.all().filter(author=author)
+
+        return categories
