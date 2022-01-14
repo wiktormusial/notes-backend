6e814fff577f503f5a3445b11707009c71994169
Wiktor Musial <wiktormusial@icloud.com>
2022-01-14 23:41:58 +0100

    Create serializers for Notes and Categories

--- note/serializers.py
+++ note/serializers.py
@@ -18,3 +18,25 @@ class NoteSerializer(serializers.ModelSerializer):
         if not self.context['request'].user.categories.filter(pk=value.pk).exists():
             raise serializers.ValidationError('invalid category')
         return value
+
+    def validate_author(self, value):
+        if not self.context['request'].user.id == value.id:
+            raise serializers.ValidationError('invalid author')
+        return value
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+    def create(self, validated_data):
+        obj = Category.objects.create(**validated_data)
+        obj.author = self.context['request'].user
+        obj.save()
+
+        return obj
+
+    def validate_author(self, value):
+        if not self.context['request'].user.id == value.id:
+            raise serializers.ValidationError('invalid author')
+        return value
