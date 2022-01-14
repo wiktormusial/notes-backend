1ce23f2223855a77c563830e38131d16ddad18fd
Wiktor Musial <wiktormusial@icloud.com>
2022-01-15 00:19:34 +0100

    Rename urls

--- note/urls.py
+++ note/urls.py
@@ -7,6 +7,6 @@ from .views import NotesViewSet, CategoriesViewSet
 urlpatterns = [
     path('notes', NotesViewSet.as_view({'get': 'list', 'post': 'create'}), name="notes"),
     path('notes/<int:pk>', NotesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="notes"),
-    path('category', CategoriesViewSet.as_view({'get': 'list', 'post': 'create'}), name="categories"),
-    path('category/<int:pk>', CategoriesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="categories")
+    path('categories', CategoriesViewSet.as_view({'get': 'list', 'post': 'create'}), name="categories"),
+    path('categories/<int:pk>', CategoriesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="categories"),
 ]
