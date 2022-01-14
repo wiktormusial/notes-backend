846e7338d7076fb637c286d31ffa4dd8d0e5124b
Wiktor Musial <wiktormusial@icloud.com>
2022-01-14 23:41:04 +0100

    Create urls

--- note/urls.py
+++ note/urls.py
@@ -3,7 +3,10 @@ from rest_framework.routers import SimpleRouter
 
 from .views import NotesViewSet, CategoriesViewSet
 
+
 urlpatterns = [
-    path('<str:token>', NotesViewSet.as_view({'get': 'list', 'post': 'create'}), name="notesallview"),
-    path('<str:token>/<int:pk>', NotesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}, name="noteseditview"))
+    path('notes', NotesViewSet.as_view({'get': 'list', 'post': 'create'}), name="notes"),
+    path('notes/<int:pk>', NotesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="notes"),
+    path('category', CategoriesViewSet.as_view({'get': 'list', 'post': 'create'}), name="categories"),
+    path('category/<int:pk>', CategoriesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="categories")
 ]
