fd951b626d010a8f868f13b6a0b9737bbe838eff
Wiktor Musial <wiktormusial@icloud.com>
2022-01-15 01:21:31 +0100

    Create SchemaView

--- docs/views.py
+++ docs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class SchemaView(TemplateView):
+    template_name = "docs/swagger-ui.html"
+    extra_context = {
+        'schema_url': 'openapi-schema'
+    }
