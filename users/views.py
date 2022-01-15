c94406e91c125c277b39b6b503b58c8f3c30d2c2
Wiktor Musial <wiktormusial@icloud.com>
2022-01-15 23:37:25 +0100

    Create UserInfoView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+class UserInfoView(APIView):
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
+    def get(self, *args, **kwargs):
+        user = self.request.user.id
+        queryset = User.objects.all().filter(id=user)
+        serializer = UserSerializer(queryset, many=True)
+
+        return Response(serializer.data)
